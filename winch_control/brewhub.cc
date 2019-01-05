594177bde926fe13ed8a94ad329231f17156e39d
Garratt <garratt@google.com>
2019-01-05 04:22:31 -0800

    dry run and winches work
    Ran the dryRun() and the program captured every stage.
    Ran steps 1-4, and the mash and hops went where they were supposed to
    The only thing left to do is get the hoses hooked up

--- winch_control/brewhub.cc
+++ winch_control/brewhub.cc
@@ -21,15 +21,27 @@
 #define RIGHT_WINCH_ENABLE 2
 #define RIGHT_WINCH_DIRECTION 3
 
-#define SET_BUTTON 7
+#define SET_BUTTON 25
 #define PUMP_BUTTON 8
 #define SPEAKER_IN 1
 
 #define RIGHT_SLIDE_SWITCH 21
 
-#define gpio_dir(pin) "/sys/class/gpio/gpio" str(s) "/direction"
-#define gpio_val(pin) "/sys/class/gpio/gpio" str(s) "/value"
-#define str(s) #s
+// #define str(s) #s
+// #define gpio_dir(pin) "/sys/class/gpio/gpio" str(pin) "/direction"
+// #define gpio_val(pin) "/sys/class/gpio/gpio" str(pin) "/value"
+
+char path_buffer[50];
+
+char *gpio_val(uint8_t pin) {
+   sprintf(path_buffer, "/sys/class/gpio/gpio%u/value", pin);
+   return path_buffer;
+}
+
+char *gpio_dir(uint8_t pin) {
+   sprintf(path_buffer, "/sys/class/gpio/gpio%u/direction", pin);
+   return path_buffer;
+}
 
 int SetOutput(uint8_t pin, uint8_t value) {
    int fd = open(gpio_val(pin), O_RDWR);
@@ -109,7 +121,7 @@ int InitIO() {
 
 #define RIGHT 0
 #define LEFT 1
-
+// down is 0
 // Reel the winch in, running for |ms| milliseconds.
 // the serial number can be specified using serial.
 int RunWinch(uint32_t ms, int side, int direction) {
@@ -137,10 +149,67 @@ int RunWinch(uint32_t ms, int side, int direction) {
    return 0;
 }
 
+// returns -1 if cannot communicate
+// 0 if not stopped
+// 1 if stopped (the limit switch is triggered)
+int IsRightSlideStop() {
+  // Active low (for now)
+  int val = ReadInput(RIGHT_SLIDE_SWITCH);
+  if (val < 0) return val;
+  if (val == 0) return 1;
+  return 0;
+}
+
+
+//Run both winches:
+// Direction: Right: 0 Left: 1
+int RunBothWinches(uint32_t ms, int direction) {
+  //if direction == right, left down, right up
+  int ldir = direction ? 1 : 0;
+  int rdir = direction? 0 : 1;
+   if(SetOutput(RIGHT_WINCH_DIRECTION, rdir)) {
+     printf("Failed to set output\n");
+     return -1;
+   }
+   if(SetOutput(LEFT_WINCH_DIRECTION, ldir)) {
+     printf("Failed to set output\n");
+     return -1;
+   }
+   usleep(100000); // let the relay click around
+   if (SetOutput(RIGHT_WINCH_ENABLE, 1) || SetOutput(LEFT_WINCH_ENABLE, 1)) {
+     printf("Failed to set output\n");
+     return -1;
+   }
+   // TODO: if moving right, stop at the right stop
+   // Check every ms.
+   uint32_t ms_counter = 0;
+   do {
+     usleep(1000);
+     ms_counter++;
+     if (direction == 0 && IsRightSlideStop()) {
+       break;
+     }
+   } while (ms_counter <= ms);
+
+   if (SetOutput(RIGHT_WINCH_ENABLE, 0) || SetOutput(LEFT_WINCH_ENABLE, 0)) {
+     printf("Failed to set output\n");
+     return -1;
+   }
+   if (SetOutput(LEFT_WINCH_DIRECTION, 0) || SetOutput(RIGHT_WINCH_DIRECTION, 0)) {
+     printf("Failed to set output\n");
+     return -1;
+   }
+   usleep(100000); // let the relay click around
+   return 0;
+}
+
+
 int LeftGoUp(uint32_t ms) {    return RunWinch(ms, 1, 1); }
 int LeftGoDown(uint32_t ms) {  return RunWinch(ms, 1, 0); }
 int RightGoUp(uint32_t ms) {   return RunWinch(ms, 0, 1); }
 int RightGoDown(uint32_t ms) { return RunWinch(ms, 0, 0); }
+int GoLeft(uint32_t ms) { return RunBothWinches(ms, 1); }
+int GoRight(uint32_t ms) { return RunBothWinches(ms, 0); }
 
 int RaiseToDrain() {
   // Assumes we are in the mash state
@@ -154,32 +223,40 @@ int RaiseToDrain() {
 }
 
 int MoveToSink() {
-  uint32_t step_size = 75;
-  int val;
-  do {
-    if (LeftGoDown(step_size)) { return -1; }
-    if (RightGoUp(step_size * 1.1)) { return -1; }
-    usleep(500000); // let it settle?  I'd like to get it to not swing so much...
+  // Raise to limit:
+  RightGoUp(300);
+  // Lower a little:
+  RightGoDown(100);
+  // Now scoot over using both winches:
+  GoRight(3000); // This will quit early when it hits the limit switch
+
+
+  // uint32_t step_size = 75;
+  // int val;
+  // do {
+    // if (LeftGoDown(step_size)) { return -1; }
+    // if (RightGoUp(step_size * 1.1)) { return -1; }
+    // usleep(500000); // let it settle?  I'd like to get it to not swing so much...
     // read the Right slide switch to see if we are done:
-    val = ReadInput(RIGHT_SLIDE_SWITCH);
-    if (step_size < 200) {
-      step_size += 25;
-    }
-  } while (val > 0);
-  if (val < 0) {
-    return -1;
-  }
+    // val = ReadInput(RIGHT_SLIDE_SWITCH);
+    // if (step_size < 200) {
+      // step_size += 25;
+    // }
+  // } while (val > 0);
+  // if (val < 0) {
+    // return -1;
+  // }
 
   // Now we are over the sink, lower away!
-  return RightGoDown(2000);
+  return RightGoDown(3000);
 }
 
 int LowerHops() {
-  return LeftGoDown(2000);
+  return LeftGoDown(3000);
 }
 
 int RaiseHops() {
-  return LeftGoUp(1800);  // go up a little less than we went down
+  return LeftGoUp(2500);  // go up a little less than we went down
 }
 
 enum FlowPath { KETTLE, CHILLER, CARBOY};
@@ -212,16 +289,111 @@ int HitButton(uint8_t button) {
   return 0;
 }
 
+class BeepTracker {
+  uint64_t start_ = 0, stop_ = 0, prev_start_ = 0, prev_stop_ = 0;
+  int prev_val_ = 1;
+  uint64_t initial_seconds_;
+  // get milliseconds since this object was initialized.
+  // This time should only be used for diffs
+  uint64_t GetTime() {
+    timespec t;
+    clock_gettime(CLOCK_MONOTONIC, &t);
+    uint64_t ret = t.tv_sec - initial_seconds_;
+    ret *= 1000;
+    ret += t.tv_nsec / 1000000;
+    return ret;
+  }
+
+  bool IsClose(uint64_t val, uint64_t target) {
+    uint64_t diff = val > target? val - target: target - val;
+    if (diff < target / 10) return true;
+    return false;
+  }
+
+  public:
+    int CheckBeep() {
+      int val = ReadInput(SPEAKER_IN);
+      if (val < 0) { return -1; }
+      // just turned on:
+      int change = val - prev_val_;
+      prev_val_ = val;
+      if (change < 0) {
+        prev_start_ = start_;
+        start_ = GetTime();
+      }
+      // Just turned off:
+      if (change > 0) {
+        prev_stop_ = stop_;
+        stop_ = GetTime();
+        return stop_ - start_;
+      }
+      return 0;
+    }
+
+  BeepTracker() {
+    timespec t;
+    clock_gettime(CLOCK_MONOTONIC, &t);
+    initial_seconds_ = t.tv_sec;
+  }
+
+    int CheckContinous() {
+      int val = CheckBeep();
+      if (val <= 0) return val;
+      // val > 0.
+      // Critera for Continous beep: 500ms on, 500ms off, 500ms on
+      if (!IsClose(stop_ - start_, 500)) return 0;
+      if (!IsClose(start_ - prev_stop_, 500)) return 0;
+      if (!IsClose(prev_stop_ - prev_start_, 500)) return 0;
+      return 1;
+    }
+
+    int CheckLongBeep() {
+      int val = CheckBeep();
+      if (val <= 0) return val;
+      if (IsClose(stop_ - start_, 1500)) {
+        return 1;
+      }
+      return 0;
+    }
+};
+
 
 int WaitForMashStart() {
   // Wait for Long beep
-  // Then wait for the set button to be pushed
-  return 0;
+  BeepTracker bt;
+  int ret;
+  do {
+    usleep(1000);
+    ret = bt.CheckLongBeep();
+  } while (ret == 0);
+  return ret;
+  // TODO: Then wait for the set button to be pushed
 }
 
+
 int WaitForBeeping() {
-  // TODO
-  return 0;
+  BeepTracker bt;
+  int ret;
+  do {
+    usleep(1000);
+    ret = bt.CheckContinous();
+  } while (ret == 0);
+  if (ret > 0) {
+    HitButton(SET_BUTTON);
+  }
+  return ret;
+}
+
+void ListenForBeeps() {
+  BeepTracker bt;
+  int ret;
+  while(1) {
+    usleep(1000);
+    ret = bt.CheckBeep();
+    if (ret > 0) {
+      printf("beep: %d\n", ret);
+    }
+  }
 }
 
 void WaitMinutes(uint32_t minutes) {
@@ -230,6 +402,29 @@ void WaitMinutes(uint32_t minutes) {
     sleep(10);
   } while (difftime(begin, time(NULL)) < minutes * 60);
 }
+
+void DryRun() {
+  printf("Waiting for mash to start...\n");
+  WaitForMashStart();
+  printf("Mash at Temp. Waiting for Mash to Finish.\n");
+  // TODO: time how long mash takes
+
+  // Wait for Mash to be done
+  WaitForBeeping();
+  printf("Mash is Done! Lift and let drain\n");
+  sleep(1);
+  printf("Skip to Boil\n");
+  HitButton(SET_BUTTON);
+  // wait for boil temp to be reached
+  
+  WaitForBeeping();
+  printf("Boil Reached\n");
+  HitButton(SET_BUTTON);
+  // Wait for Boil to be done
+  WaitForBeeping();
+  printf("Boil Done\n");
+}
+
 
 void RunTestCommand(int argc, char **argv) {
   if (argc < 2) return;
@@ -240,7 +435,37 @@ void RunTestCommand(int argc, char **argv) {
   }
 
   if (argv[1][0] == 'P') {
-    HitButton(SET_BUTTON);
+    HitButton(PUMP_BUTTON);
+    return;
+  }
+
+  if (argv[1][0] == 'd') {
+    DryRun();
+    return;
+  }
+
+  if (argv[1][0] == 'L') {
+    ListenForBeeps();
+    return;
+  }
+
+  if (argv[1][0] == '1') {
+    RaiseToDrain();
+    return;
+  }
+
+  if (argv[1][0] == '2') {
+    MoveToSink();
+    return;
+  }
+
+  if (argv[1][0] == '3') {
+    LowerHops();
+    return;
+  }
+
+  if (argv[1][0] == '4') {
+    RaiseHops();
     return;
   }
 
@@ -249,35 +474,43 @@ void RunTestCommand(int argc, char **argv) {
     int slide = ReadInput(RIGHT_SLIDE_SWITCH);
     for(int i = 0; i < 5; ++i) {
       printf("Right Slide Switch: %d\n", slide);
-      sleep(1);
+      usleep(1001);
     }
     return;
   }
-
-  if (argv[1][0] == 'l' || argv[1][0] == 'r' ) {
-    // Default to 200ms, which is ~8cm.
-    uint32_t ms = 200;  
-    if (argc > 2) {
-      int ms_in = atoi(argv[2]);
-      ms = (ms_in < 0) ? 0 : ms_in;
-    }
-  
-    if (argv[1][0] == 'l' && argv[1][1] == 'u') {
-      LeftGoUp(ms);
-    }
-  
-    if (argv[1][0] == 'l' && argv[1][1] == 'd') {
-      LeftGoDown(ms);
-    }
-
-    if (argv[1][0] == 'r' && argv[1][1] == 'u') {
-      RightGoUp(ms);
-    }
-
-    if (argv[1][0] == 'r' && argv[1][1] == 'd') {
-      RightGoDown(ms);
-    }
+  // Default to 200ms, which is ~8cm.
+  uint32_t ms = 200;  
+  if (argc > 2) {
+    int ms_in = atoi(argv[2]);
+    ms = (ms_in < 0) ? 0 : ms_in;
   }
+
+
+  if (argv[1][0] == 'l' && argv[1][1] == 'u') {
+    LeftGoUp(ms);
+  }
+
+  if (argv[1][0] == 'l' && argv[1][1] == 'd') {
+    printf("Left doing down\n");
+    LeftGoDown(ms);
+  }
+
+  if (argv[1][0] == 'r' && argv[1][1] == 'u') {
+    RightGoUp(ms);
+  }
+
+  if (argv[1][0] == 'r' && argv[1][1] == 'd') {
+    RightGoDown(ms);
+  }
+
+  if (argv[1][0] == 'b' && argv[1][1] == 'l') {
+    RunBothWinches(ms, 1);
+  }
+
+  if (argv[1][0] == 'b' && argv[1][1] == 'r') {
+    RunBothWinches(ms, 0);
+  }
+
 }
 
 int main(int argc, char **argv) {
@@ -311,23 +544,23 @@ int main(int argc, char **argv) {
   HitButton(PUMP_BUTTON);
   WaitMinutes(45);
 
-  SetFlow(FlowPath::CHILLER);
+  SetFlow(CHILLER);
 
   // TODO: wait for 10 minutes
   WaitMinutes(10);
 
-  SetFlow(FlowPath::CARBOY);
+  SetFlow(CARBOY);
   //Wait for 2 minutes
   WaitMinutes(2);
 
-  SetFlow(FlowPath::KETTLE);
+  SetFlow(KETTLE);
 
   // Wait for boil to complete:
   WaitForBeeping();
 
   RaiseHops();
 
-  SetFlow(FlowPath::CHILLER);
+  SetFlow(CHILLER);
   ActivateChillerPump();
   HitButton(PUMP_BUTTON);
 
@@ -340,7 +573,7 @@ int main(int argc, char **argv) {
   WaitMinutes(1);
 
   // Decant:
-  SetFlow(FlowPath::CARBOY);
+  SetFlow(CARBOY);
   HitButton(PUMP_BUTTON);
   WaitMinutes(20);
   // Wait for 20 minutes
