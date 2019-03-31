a7b8fb26d89e4bb988814cec3b620bbd3d540ffb
Garratt <garratt@google.com>
2019-03-31 16:15:54 -0700

    updating scale test to new api

--- winch_control/scale_test.cc
+++ winch_control/scale_test.cc
@@ -9,11 +9,17 @@ void PrintWeight(double grams, int64_t wtime) {
   printf("Weight: %4.5lf   time: %ld\n", grams, wtime);
 }
 
+bool global_error = false;
+void ErrorFunc() {
+  printf("Error function called, ending loop!\n");
+  global_error=true;
+}
 
 int main(int argc, char **argv) {
    ScaleFilter sf("calibration.txt");
+   sf.InitLoop(&ErrorFunc);
    sf.SetPeriodicWeightCallback(1000, &PrintWeight);
- while (1) {
+ while (!global_error) {
     sleep(10);
  }
   return 0;
